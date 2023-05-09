3d73d69b4f1df1842653ac858486da70d8774aab
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-09 17:13:16 +0200

    Back

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { google } = require('googleapis');
+const { OAuth2Client } = require("google-auth-library");
 const express = require('express')
 const OAuth2Data = require('./google_key.json')
 
@@ -26,8 +27,8 @@ app.get('/', (req, res) => {
             }
             res.send('Logged in: '.concat(loggedUser, ' <img src = "', result.data.picture, '" height="23" width="23"> <br> <a href="#" onclick="signOut();">Sign out</a>' +
                 '<script> function signOut() { var auth2 = gapi.auth2.getAuthInstance(); auth2.signOut().then(function () {' +
-                'console.log("User signed out."); authed = false; window.location.replace("/");' +
-                '}); window.location.href = "https://pki-lab6-tht8.onrender.com/login";}</script>'));
+                'console.log("User signed out."); authed = false; window.location.replace("pki-lab6-tht8.onrender.com/login");' +
+                '}); window.location.href = "pki-lab6-tht8.onrender.com/login";}</script>'));
 
         })
     }
