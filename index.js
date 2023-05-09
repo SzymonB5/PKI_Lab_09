b798f00eece69cc699155381e59dcc3cac5a4516
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-09 16:53:41 +0200

    Fix

--- index.js
+++ index.js
@@ -8,97 +8,38 @@ const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
-
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
 
-const getUsers = (request, response) => {
-    console.log('Pobieram dane ...');
-    client.query('SELECT * FROM public."Users"', (error, res) => {
-        if (error) {
-            throw error
-        }
-        console.log('Dostałem ...');
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-    })
-}
-
-const addOrUpdateUser = (email) => {
-    const joined = new Date().toISOString();
-    const lastvisit = joined;
-    const counter = 1;
-    const query = 'SELECT * FROM public."Users" WHERE email = $1';
-    client.query(query, [email], (error, result) => {
-        if (error) {
-            throw error;
-        }
-        if (result.rows.length === 0) {
-            const insertQuery = 'INSERT INTO public."Users" (email, joined, lastvisit, counter) VALUES ($1, $2, $3, $4)';
-            client.query(insertQuery, [email, joined, lastvisit, counter], (error, result) => {
-                if (error) {
-                    throw error;
-                }
-                console.log('User added to database');
-            });
-        } else {
-            const updateQuery = 'UPDATE public."Users" SET lastvisit = $1, counter = counter + 1 WHERE email = $2';
-            client.query(updateQuery, [lastvisit, email], (error, result) => {
-                if (error) {
-                    throw error;
-                }
-                console.log('User updated in database');
-            });
-        }
-    });
-}
-
 app.get('/', (req, res) => {
-	console.log(authed);
     if (!authed) {
-        // Generate an OAuth URL and redirect there
-        const url = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/gmail.readonly'
-        });
-        console.log(url)
-        res.redirect(url);
+        res.redirect('/login');
     } else {
-        PGHOST='ep-wild-bush-363371.eu-central-1.aws.neon.tech'
-        PGDATABASE='neondb'
-        PGUSER='SzymonB5'
-        PGPASSWORD='<% PASSWORD %>'
-        ENDPOINT_ID='ep-wild-bush-363371'
-
-
-        const postgres = require('postgres');
-        require('dotenv').config();
-
-        getUsers();
-        const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-        const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
-        addOrUpdateUser();
-
-        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-        gmail.users.labels.list({
-            userId: 'me',
-        }, (err, res) => {
-            if (err) return console.log('The API returned an error: ' + err);
-            const labels = res.data.labels;
-            if (labels.length) {
-                console.log('Labels:');
-                labels.forEach((label) => {
-                    console.log(`- ${label.name}`);
-                });
+        var oauth2 = google.oauth2({auth: oAuth2Client, version: 'v2'});
+        oauth2.userinfo.v2.me.get(function (err, result) {
+            if (err) {
+                console.log('ERROR');
+                console.log(err);
             } else {
-                console.log('No labels found.');
+                loggedUser = result.data.name;
+                console.log(loggedUser);
             }
-        });
-        res.send('Logged in')
+            res.send('Logged in: '.concat(loggedUser, ' <img src = "', result.data.picture, '" height="23" width="23"> <br> <a href="#" onclick="signOut();">Sign out</a>' +
+                '<script> function signOut() { var auth2 = gapi.auth2.getAuthInstance(); auth2.signOut().then(function () {' +
+                'console.log("User signed out."); authed = false; window.location.replace("/");' +
+                '}); window.location.href = "/login";}</script>'));
+
+        })
     }
-})
+});
+
+app.get('/login', (req, res) => {
+    const url = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile'
+    });
+    res.send('<a href="' + url + '">Login with Google</a>');
+});
 
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
